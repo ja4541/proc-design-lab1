3fb5f4f20a0fe90a83906ced71b7a2978e1c3e40
Josef <ja4541@nyu.edu>
2025-11-09 14:09:59 -0500

    Clear lab files to start notes

--- include/name.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef NAME_HPP
-#define NAME_HPP
-
-#include <string>
-#include <iostream>
-
-std::string getUserName();
-
-#endif // NAME_HPP
--- src/name.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-
-std::string getUserName() {
-    std::cout << "Please enter your name: ";
-    std::string name;
-    std::cin >> name;
-    return name;
-}
